6046f729ed55185a1f802723708b5fde150fb427
Joost de Valk <joost@altha.nl>
2023-11-28 21:49:35 +0100

    Add playground code

---
+++ fewer-tags-playground.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ * File that creates a better playground environment for the Fewer Tags plugin.
+ *
+ * @package Fewer_Tags_Playground
+ */
+
+ if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Class Joost_Fewer_Tags_Playground
+ */
+class Joost_Fewer_Tags_Playground {
+	/**
+	 * Class instance.
+	 *
+	 * @var Joost_Fewer_Tags_Playground
+	 */
+	private static $instance = false;
+
+	/**
+	 * Class constructor.
+	 */
+	public function __construct() {
+		add_action( 'admin_notices', [ $this, 'admin_notices' ] );
+	}
+
+	/**
+	 * Print an admin notice helping people.
+	 */
+	public function admin_notices() {
+		$screen = \get_current_screen();
+		if ( ! \is_object( $screen ) || $screen->base !== 'edit-tags' ) {
+			return;
+		}
+
+		echo '<div id="fewer-tags-playground-notice" class="notice notice-success">';
+		echo '<p><strong>' . esc_html__( 'Fewer Tags demo', 'fewer-tags' ) . '</strong><br>';
+		esc_html_e( 'This is a demo of the Fewer Tags plugin. As you can see if you hover over them, tags that don\'t hit the required count, do not have a view action. That is because they are not live on the site.', 'fewer-tags' );
+		echo '</p>';
+		echo '<p>';
+		esc_html_e( 'You can go to Settings → Reading to change how many tags a tag needs to have to be live on the site.', 'fewer-tags' );
+		echo '</p>';
+		echo '</div>';
+	}
+
+	/**
+	 * Get the instance of the class.
+	 */
+	public static function get_instance() {
+		if ( ! self::$instance ) {
+			self::$instance = new Joost_Fewer_Tags_Playground();
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * Generate random posts.
+	 */
+	public function generate_posts() {
+		$common_tag  = $this->create_tag( 'Common Tag' );
+		$special_tag = $this->create_tag( 'Special Tag' );
+
+		$post_ids = [];
+		for ( $i = 0; $i < 20; $i++ ) {
+			$post_ids[] = $this->create_random_post( $common_tag );
+		}
+
+		// Attach 'Special Tag' to 5 random posts.
+		$selected_posts = array_rand( $post_ids, 5 );
+		foreach ( $selected_posts as $post_id ) {
+			wp_set_object_terms( $post_ids[ $post_id ], $special_tag, 'post_tag', true );
+		}
+	}
+
+	/**
+	 * Create a random post.
+	 *
+	 * @param string $common_tag Tag to attach to all posts.
+	 * @return int Post ID.
+	 */
+	private function create_random_post( $common_tag ) {
+		$postarr = [
+			'post_title'   => $this->create_random_string( 5 ),
+			'post_content' => $this->create_random_string( 10 ),
+			'post_status'  => 'publish',
+			'post_type'    => 'post',
+			'tags_input'   => [ $common_tag ],
+		];
+
+		return wp_insert_post( $postarr );
+	}
+
+	/**
+	 * Create a tag.
+	 *
+	 * @param string $tag_name Name of the tag.
+	 *
+	 * @return string Tag name.
+	 */
+	private function create_tag( $tag_name ) {
+		if ( ! term_exists( $tag_name, 'post_tag' ) ) {
+			$term = wp_insert_term( $tag_name, 'post_tag' );
+			return $term['term_id'];
+		}
+	}
+
+	/**
+	 * Create a random string of content.
+	 *
+	 * @param int $length Length of the string to create.
+	 *
+	 * @return string Random string.
+	 */
+	private function create_random_string( $length ) {
+		$words     = [ 'the', 'and', 'have', 'that', 'for', 'you', 'with', 'say', 'this', 'they', 'but', 'his', 'from', 'not', 'she', 'as', 'what', 'their', 'can', 'who', 'get', 'would', 'her', 'all', 'make', 'about', 'know', 'will', 'one', 'time', 'there', 'year', 'think', 'when', 'which', 'them', 'some', 'people', 'take', 'out', 'into', 'just', 'see', 'him', 'your', 'come', 'could', 'now', 'than', 'like', 'other', 'how', 'then', 'its', 'our', 'two', 'more', 'these', 'want', 'way', 'look', 'first', 'also', 'new', 'because', 'day', 'use', 'man', 'find', 'here', 'thing', 'give', 'many', 'well', 'only', 'those', 'tell', 'very', 'even', 'back', 'any', 'good', 'woman', 'through', 'life', 'child', 'work', 'down', 'may', 'after', 'should', 'call', 'world', 'over', 'school', 'still', 'try', 'last', 'ask', 'need' ];
+		$word_keys = array_rand( $words, $length );
+		$sentence  = '';
+		foreach ( $word_keys as $key ) {
+			$sentence .= $words[ $key ] . ' ';
+		}
+		return ucfirst( trim( $sentence ) ) . '.';
+	}
+}
+
+// Usage.
+$joost_fewer_tags_playground = new Joost_Fewer_Tags_Playground();
--- readme.txt
+++ readme.txt
@@ -14,11 +14,11 @@ This plugin minimizes the effect of having too many tags by setting a minimum nu
 One of the most common SEO problems on WordPress sites is that people add too many tags to posts. The WordPress interface makes it incredibly easy to do so, and with every tag you add, you add another URL to your site for search engines to crawl and index. This plugin minimizes that effect by setting a minimum number of posts needed for a tag to be “live” on your site.
 
 Tags that have less than the configured number of posts:
- * don\'t work on your site, and are redirected to your homepage.
+ * don't work on your site, and are redirected to your homepage.
  * no longer show up in tag listings.
  * are no longer linked in WordPress core's or Yoast SEO generated XML sitemaps.
 
-This positively affects your site\'s SEO and also leads to less crawling, as you have less useless tag pages.
+This positively affects your site's SEO and also leads to less crawling, as you have less useless tag pages.
 
 == Frequently Asked Questions ==
 
@@ -35,13 +35,13 @@ No, you should not noindex your tag pages. If your tag pages have more than 10 p
 You can report security bugs through the Patchstack Vulnerability Disclosure Program. The Patchstack team help validate, triage and handle any security vulnerabilities. [Report a security vulnerability.](https://patchstack.com/database/vdp/fewer-tags)
 
 == Installation ==
-1. Search for Fewer tags on the repository
-2. Install the plugin
-3. Optionally: go to Settings -> Reading and adjust the default minimum number of posts (the default is 10).
-4. You\'re done
+1. Search for Fewer tags on the repository.
+2. Install the plugin.
+3. Optionally: go to Settings → Reading and adjust the default minimum number of posts (the default is 10).
+4. You're done.
 
 == Screenshots ==
-1. The Fewer tags settings on the Options -> Reading screen of the settings.
+1. The Fewer tags settings on the Settings → Reading screen.
 2. Fewer Tags adds a column to the Tags overview page, showing which tags are live and which aren't.
 
 == Changelog ==
