19da61a6e1f77068cf157dc3a367aed6fa48c41d
Joost de Valk <joost@altha.nl>
2024-01-22 13:37:36 +0100

    Prefix variable

--- uninstall.php
+++ uninstall.php
@@ -10,9 +10,9 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	die;
 }
 
-$ft_option_name = 'joost_min_posts_count';
+$fewer_tags_option_name = 'joost_min_posts_count';
 
-delete_option( $ft_option_name );
+delete_option( $fewer_tags_option_name );
 
 // For site options in Multisite.
-delete_site_option( $ft_option_name );
+delete_site_option( $fewer_tags_option_name );
